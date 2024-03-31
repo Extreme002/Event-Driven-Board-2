8234ca4a162c122b2e61d0a58e032298dde3a5ad
Extreme002 <alexpietras0@gmail.com>
2024-03-31 16:40:37 +0100

    Removed the sub class
    Not what I want to do but it works. The sub classes are now removed and are physically in the main class.

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import tkinter as tk
 import random
+import time
 
-class GridSquare:
-    def __init__(self, root, color, start_row, start_col):
+class GridAI:
+    def __init__(self, root):
         self.root = root
-        self.color = color
-        self.current_row = start_row
-        self.current_col = start_col
+        self.root.title("Grid Layout")
         self.grid_buttons = []
+        self.current_row_green = 5
+        self.current_col_green = 5
+        self.current_row_orange = 2
+        self.current_col_orange = 2
+
         self.create_grid()
+        self.root.after(1000, self.move_green)
+        self.root.after(1500, self.move_orange)
 
     def create_grid(self):
         for i in range(10):
@@ -19,56 +25,66 @@ class GridSquare:
                 row.append(button)
             self.grid_buttons.append(row)
 
-        # Set starting position
-        self.grid_buttons[self.current_row][self.current_col]['bg'] = self.color
+        # Set starting positions
+        self.grid_buttons[self.current_row_green][self.current_col_green]['bg'] = 'green'
+        self.grid_buttons[self.current_row_orange][self.current_col_orange]['bg'] = 'orange'
 
     def toggle_color(self, row, col):
         current_color = self.grid_buttons[row][col]['bg']
         new_color = 'black' if current_color == 'white' else 'white'
         self.grid_buttons[row][col]['bg'] = new_color
 
-    def move(self):
-        options = self.get_available_moves()
-        if options:
-            new_row, new_col = random.choice(options)
-            self.grid_buttons[self.current_row][self.current_col]['bg'] = 'white'
-            self.current_row, self.current_col = new_row, new_col
-            self.grid_buttons[self.current_row][self.current_col]['bg'] = self.color
-        self.root.after(1000, self.move)
+    def move_green(self):
+        self.highlight_adjacent_squares(self.current_row_green, self.current_col_green)
+        self.root.after(1000, self.select_move_green)
 
-    def get_available_moves(self):
-        options = []
+    def move_orange(self):
+        self.highlight_adjacent_squares(self.current_row_orange, self.current_col_orange)
+        self.root.after(1000, self.select_move_orange)
+
+    def highlight_adjacent_squares(self, row, col):
+        # Highlight adjacent squares in light blue
         for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-            new_row, new_col = self.current_row + dr, self.current_col + dc
+            new_row, new_col = row + dr, col + dc
             if 0 <= new_row < 10 and 0 <= new_col < 10:
                 if self.grid_buttons[new_row][new_col]['bg'] == 'white':
+                    self.grid_buttons[new_row][new_col]['bg'] = 'light blue'
+
+    def select_move_green(self):
+        options = self.get_available_moves(self.current_row_green, self.current_col_green)
+        if options:
+            new_row, new_col = random.choice(options)
+            self.grid_buttons[self.current_row_green][self.current_col_green]['bg'] = 'white'
+            self.current_row_green, self.current_col_green = new_row, new_col
+            self.grid_buttons[self.current_row_green][self.current_col_green]['bg'] = 'green'
+        self.root.after(1000, self.deselect_squares)
+
+    def select_move_orange(self):
+        options = self.get_available_moves(self.current_row_orange, self.current_col_orange)
+        if options:
+            new_row, new_col = random.choice(options)
+            self.grid_buttons[self.current_row_orange][self.current_col_orange]['bg'] = 'white'
+            self.current_row_orange, self.current_col_orange = new_row, new_col
+            self.grid_buttons[self.current_row_orange][self.current_col_orange]['bg'] = 'orange'
+        self.root.after(1000, self.deselect_squares)
+
+    def get_available_moves(self, row, col):
+        options = []
+        for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            new_row, new_col = row + dr, col + dc
+            if 0 <= new_row < 10 and 0 <= new_col < 10:
+                if self.grid_buttons[new_row][new_col]['bg'] == 'light blue':
                     options.append((new_row, new_col))
         return options
 
-class BlueSquare(GridSquare):
-    def __init__(self, root):
-        super().__init__(root, 'light blue', 0, 0)
-
-    def move(self):
-        self.highlight_available_moves()
-        self.root.after(1000, self.move)
-
-    def highlight_available_moves(self):
+    def deselect_squares(self):
         for i in range(10):
             for j in range(10):
                 if self.grid_buttons[i][j]['bg'] == 'light blue':
                     self.grid_buttons[i][j]['bg'] = 'white'
-
-        for square in [green_square, orange_square]:
-            options = square.get_available_moves()
-            for row, col in options:
-                self.grid_buttons[row][col]['bg'] = 'light blue'
+        self.root.after(1000, self.move_green)
+        self.root.after(1000, self.move_orange)
 
 root = tk.Tk()
-green_square = GridSquare(root, 'green', 5, 5)
-orange_square = GridSquare(root, 'orange', 2, 2)
-blue_square = BlueSquare(root)
-root.after(1000, green_square.move)
-root.after(1500, orange_square.move)
-root.after(2000, blue_square.move)
+ai = GridAI(root)
 root.mainloop()
